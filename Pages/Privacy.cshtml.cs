b30ff99b3548cae6c3a02c03750a4ca7d4a7f5ff
yatish <ydipankar@gmail.com>
2023-05-26 23:27:14 -0400

    inital commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCoreApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+        ViewData["TimeStamp"] = dateTime;
+    }
+}
+
+
